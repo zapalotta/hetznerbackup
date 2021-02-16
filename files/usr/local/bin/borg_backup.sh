8ae8468f02c0f1352222c34ebf9e8a52a16fa892
Dirk Jugov <dirk@jugov.de>
2021-02-16 21:23:09 +0000

    Cleanup and docs

--- files/usr/local/bin/borg_backup.sh
+++ files/usr/local/bin/borg_backup.sh
@@ -1,55 +1,29 @@
 #!/usr/bin/env bash
 
-##
-## Setzten von Variablen
-##
-
-
+# Source config file
 
 . /etc/borg_backup.conf
 
 
-##
-## Setzten von Umgebungsvariablen
-##
-
-## falls nicht der Standard SSH Key verwendet wird können
-## Sie hier den Pfad zu Ihrem private Key angeben
-# export BORG_RSH="ssh -i /home/userXY/.ssh/id_ed25519"
-
-
-# some helpers and error handling:
-info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
-trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+info() { 
+	printf "\n%s %s\n\n" "$( date )" "$*" >&2; 
+}
 
 
 
-## Damit das Passwort vom Repository nicht eingegeben werden muss
-## kann es in der Umgepungsvariable gesetzt werden
-#export BORG_RSH='ssh -i /root/.ssh/id_rsa_hetznerbackup-sub2'
 export BORG_RSH="${REPOSITORY_KEY}"
-
 export BORG_PASSPHRASE="${REPOSITORY_PASS}"
 
-## Hinweis: Für die Verwendung mit einem Backup-Account muss
-## 'your-storagebox.de' in 'your-backup.de' geändert werden.
-
 REPOSITORY="ssh://${BACKUP_USER}@${REPOSITORY_HOST}:${REPOSITORY_PORT}/./${REPOSITORY_DIR}"
-
-#echo $REPOSITORY
-
-#exit
-
-##
-## Ausgabe in Logdatei schreiben
-##
 
 exec > >(tee -i ${LOG})
 exec 2>&1
 
-echo "###### Backup gestartet: $(date) ######"
+echo "====== Starting Backup: $(date) ======"
+info "====== Starting Backup: $(date) ======"
 
-info "###### Backup gestartet: $(date) ######"
+
+# Create list of exclude dirs
 
 EXCLUDE_ARRAY=$(echo $EXCLUDE_FOLDERS | tr ' ' '\n')
 EXCLUDE_PARAMS=""
@@ -58,52 +32,13 @@ do
   EXCLUDE_PARAMS="$EXCLUDE_PARAMS --exclude ${param}"
 done
 
-
-##
-## An dieser Stelle können verschiedene Aufgaben vor der
-## Übertragung der Dateien ausgeführt werden, wie z.B.
-##
-## - Liste der installierten Software erstellen
-## - Datenbank Dump erstellen
-##
-
-##
-## Dateien ins Repository übertragen
-## Gesichert werden hier beispielsweise die Ordner root, etc,
-## var/www und home
-## Ausserdem finden Sie hier gleich noch eine Liste Excludes,
-## die in kein Backup sollten und somit per default ausgeschlossen
-## werden.
-##
-
-echo "Übertrage Dateien ..."
+# place pre-backup-stuff here
 
 borg create $REPOSITORY::"{$BACKUP_TIMESTAMP}" $INCLUDE_FOLDERS $EXCLUDE_PARAMS -v --stats 2>&1
 
-#echo borg create                    \
-#    $REPOSITORY::'{now:%Y-%m-%d_%H:%M}'  \
-#    /root                                \
-#    /etc                                 \
-#    /var				 \
-#    /boot				 \
-#    /home                                \
-#    /opt				 \
-#    --exclude /dev                       \
-#    --exclude /proc                      \
-#    --exclude /sys                       \
-#    --exclude /var/run                   \
-#    --exclude /run                       \
-#    --exclude /lost+found                \
-#    --exclude /mnt                       \
-#    --exclude /var/lib/libvirt           \
-#    -v --stats 2>&1
+echo "====== Backup finished: $(date) ======"
 
-
-# backup_exit=$?
-
-echo "###### Backup beendet: $(date) ######"
-
-echo "###### Pruning Backup: $(date) ######"
+echo "====== Pruning Backup: $(date) ======"
 
 info "Pruning main repository"
 
@@ -115,12 +50,18 @@ borg prune                          \
     --keep-monthly  $KEEP_MONTHLY   \
     $REPOSITORY
 
-# prune_exit=$?
+echo "====== Pruning finished: $(date) ======"
+
+
+echo "====== Status: $(date) ======"
 
 borg list $REPOSITORY
 
+# create statusfiles for monitoring
+borg list $REPOSITORY --short > $BORG_STATUSFILE 2>/dev/null
+borg info $REPOSITORY::$(tail -n1 /var/borgstatus) > ${BORG_STATUSFILE}_info 2>/dev/null
+
 # Unset pass
 export BORG_PASSPHRASE=''
-
 
 
